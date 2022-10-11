a3829b36aa76389ade1c62d0f3acc34b1daa9eaa
H0rn3t Sp1d3rs <97798085+H0rn3t-Sp1d3rs@users.noreply.github.com>
2022-10-12 02:35:02 +0600

    Create ip.py

---
+++ ip.py
@@ -0,0 +1,52 @@
+#Assalamu wlaikum
+#Code By H0rn3t sp1d3rs
+#team bads security researchers
+import socket
+x='''\033[1;36;40m
+██╗██████╗░
+██║██╔══██╗
+██║██████╔╝
+██║██╔═══╝░
+██║██║░░░░░
+╚═╝╚═╝░░░░░
+
+███████╗██╗███╗░░██╗██████╗░
+██╔════╝██║████╗░██║██╔══██╗
+█████╗░░██║██╔██╗██║██║░░██║
+██╔══╝░░██║██║╚████║██║░░██║
+██║░░░░░██║██║░╚███║██████╔╝
+╚═╝░░░░░╚═╝╚═╝░░╚══╝╚═════╝░
+'''
+print("\033[1;34;40m=============================================")
+print("\033[1;33;40m|     DEVELOPED BY MAD HACKERS COMMUNITY    |")
+print("\033[1;34;40m=============================================")
+print(x)
+print("\033[1;34;40m=============================================")
+print("\033[1;33;40m|     DEVELOPED BY MAD HACKERS COMMUNITY    |")
+print("\033[1;34;40m=============================================")
+
+print("\033[1;33;40mCODE NAME: H0rn3t Sp1d3rs  ")
+print("\033[1;33;40mAuthor : Mad Hackers Community")
+print("GITHUB : HTTPS://GITHUB.COM/H0rn3t-Sp1d3rs")
+print("Website : https://www.h0rn3t-sp1d3rs.tk")
+
+print("\033[1;34;40m=============================================")
+print("\033[1;33;40m|     DEVELOPED BY MAD HACKERS COMMUNITY    |")
+print("\033[1;34;40m=============================================")
+
+file = open('url.txt').read().split()
+for url in file:
+    domain = url.split('//')[1].replace('www.', '').replace('/', '')
+    try :
+        ipadrre = socket.gethostbyname(domain)
+        
+        print("\033[1;32;40m ")
+        print(ipadrre)
+        
+        for urldata in ipadrre:
+            with open("ip.txt", "a") as f:
+                f.write(urldata+"\n")
+                            
+    except:
+        pass
+
